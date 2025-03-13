9cd1cbfd46f1c568ef10de83033445cc7969c0c8
nekena123 <rakotomanjatonekena@gmail.com>
2025-03-13 20:14:46 +0300

    initialize

---
+++ src/main/java/com/example/EvaluationTest/EvaluationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.EvaluationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EvaluationTest/EvaluationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EvaluationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
